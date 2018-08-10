445a39754df295408a34c245c3749d272b5f7046
ZhehaoLi9705 <511267512@qq.com>
2018-08-10 19:59:51 +0800

    Daily Update

--- backtracking.py
+++ backtracking.py
@@ -83,44 +83,44 @@ def BasicParameters(cbond_data):
     return bond_coupon,Time,price,number
     
 
-def StockVolatility(stock_data, start_day, period=252): #计算每天正股波动率
+###############################################################################
+
+
+def StockVolatility(stock_data,period=20): #计算每天正股波动率
     #股票日涨跌幅, stock_data 是dataframe格式
-    stock_data.columns = pd.DatetimeIndex(stock_data.columns)
-    stock_change = stock_data.iloc[:,1:]/stock_data.shift(periods=1,axis=1).iloc[:,1:] - 1
-    #转债起息日起的时间序列
-    Datetime = pd.date_range(start=start_day, end=stock_change.columns[-1],freq='D')
-    #计算指数移动加权平均值
-    EWMA_avg = []
-    for i in range(Datetime.shape[0]):
-        temp_change = stock_change.T[Datetime[i]>stock_change.columns] #series
-        temp_change = temp_change.values #numpy_array
-        temp_std = np.std(temp_change)
-        volatility.append(temp_std*np.sqrt(period))
-    volatility = np.array(volatility)  
-    volatility_data = pd.DataFrame(data=volatility,index=Datetime)
-    volatility_data = volatility_data.reset_index()
-    volatility_data.columns = ['TRADE_DT','SIGMA']
-    return volatility_data
+    stock_return = stock_data.iloc[:,1:]/stock_data.shift(periods=1,axis=1).iloc[:,1:] - 1
+    stock_return = stock_return.T
+    #EWMA模型计算指数移动加权平均值
+    EWMA_Std = stock_return.ewm(span = period).std()
+    EWMA_Std = EWMA_Std*np.sqrt(250)
+    return EWMA_Std
 
 
 
-def MarkToMarket(nationbond,stock_price,Time):
+def MarkToMarket(nationbond,stock_price, cbond_price,Time): #跟踪转债上市后ß每日数据
     #处理正股价格数据
-    stock_data = stock_price.T
+    stock_data = stock_price.T # stock_price （历史两年数据）
     stock_data = stock_data.reset_index()
     stock_data.columns = ['TRADE_DT','PRICE']
     stock_data['TRADE_DT'] = pd.DatetimeIndex(stock_data['TRADE_DT'])
     #计算TTM年化波动率
-    volatility_data = StockVolatility(stock_price, Time['start'], period=252)
+    volatility_data = StockVolatility(stock_price)
+    volatility_data = volatility_data.reset_index()
+    volatility_data.columns = ['TRADE_DT','VOLATILITY']
     #处理六年期国债数据
     nationbond_data = nationbond.T
     nationbond_data = nationbond_data.reset_index()
     nationbond_data.columns = ['TRADE_DT','INTEREST']
     nationbond_data['TRADE_DT'] = pd.DatetimeIndex(nationbond_data['TRADE_DT'])
-    #波动率数据和正股价格数据合并
+    #处理债券价格是数据
+    cbond_data = cbond_price.T
+    cbond_data = cbond_data.reset_index()
+    cbond_data.columns = ['TRADE_DT','CBOND_PRICE']
+    cbond_data['TRADE_DT'] = pd.DatetimeIndex(cbond_data['TRADE_DT'])
+    #波动率数据,正股价格数据,转债价格,国债利率/无风险利率数据合并合并
     update_data = pd.merge(volatility_data, stock_data, how='left', on='TRADE_DT')
-    update_data = pd.merge(update_data, nationbond_data, how='left',on='TRADE_DT')
-    update_data = update_data.dropna()
+    update_data = pd.merge(update_data, nationbond_data, how='left', on='TRADE_DT')
+    update_data = pd.merge(cbond_data, update_data, how='left', on='TRADE_DT')
     return update_data   
 
 
@@ -129,7 +129,14 @@ def MarkToMarket(nationbond,stock_price,Time):
 
 # 计算每日delta 
 
-def ComputeDelta(TEST_price,BSM_price,Time,Price):
+def Coefficient(LSM,BSM):
+    coef_list = np.array(LSM)/np.array(BSM)
+    coef = np.mean(coef_list)
+    return coef
+
+
+
+def ComputeDelta(LSM_price,BSM_price,Time,Price):
     T0 = Time['now']
     T = Time['end']
     period = np.float(str(T-T0)[:-14])/365
@@ -137,12 +144,31 @@ def ComputeDelta(TEST_price,BSM_price,Time,Price):
     S0 = Price['now']
     K = Price['strike']
     sigma = Price['volatility']
-    FV = Price['facevalue']
-    coef_list = np.array(BSM_price)/np.array(TEST_price)
-    coef = np.mean(coef_list)
+    #确定LSM模型和BSM模型之间的比例系数
+    coef = Coefficient(LSM_price,BSM_price)
     d1 = (np.log(S0/K) + (R + 0.5*sigma**2) * period)/(sigma * np.sqrt(period))
     delta = st.norm.cdf(d1) * coef
     return delta
+
+
+def ComputeSigma(LSM_price,BSM_price,Price,Time,CBond_Price,Bond_Value):
+
+    T0 = Time['now']
+    T = Bond_Coupon.index[-1]
+    S0 = Price['now']
+    FV = Price['facevalue']
+    K = Price['strike']
+    R = Price['riskfree']
+    BV = Bond_Value
+    CBP = CBond_Price 
+    period = np.float(str(T-T0)[:-14])/365
+    #确定LSM模型和BSM模型之间的比例系数
+    coef = Coefficient(LSM_price,BSM_price)
+    def okfine(sigma):
+        return (S0 * st.norm.cdf((np.log(S0/K) + (R + 0.5*sigma**2) * period)/(sigma * np.sqrt(period))) - K * np.exp(-R*period) * st.norm.cdf((np.log(S0/K) + (R - 0.5*sigma**2) * period)/(sigma * np.sqrt(period))))* FV/K * coef + BV - CBP
+    sigma = fsolve(okfine,1)
+    return sigma
+
 
 
 ##################################################################################
@@ -155,38 +181,50 @@ def ExecuteModel(Time,Price,Bond_Coupon,Number,everyday_data):
     BSM_Price = []
     BOND_Price = []
     Delta = []
-    for day in everyday_data.columns[-2:]:
-        Time['now'] = day
-        Price['volatility'] = everyday_data[day][0]
-        Price['now'] = everyday_data[day][1]
-        Price['riskfree'] = everyday_data[day][2]/100
+    Sigma = []
+    for d in range(119,121):
+        Time['now'] = everyday_data.index[d]
+        Price['volatility'] = everyday_data['VOLATILITY'][d]
+        Price['now'] = everyday_data['PRICE'][d]
+        Price['riskfree'] = everyday_data['INTEREST'][d]/100
+        print(Time)
+        print(Price)
+        
         #调用CBond类计算模型定价
         obj = CBond(Time,Price,Bond_Coupon,Number,5000)
         value = obj.Summary()
         LSM_Price.append(value['LSM定价:'])
         BSM_Price.append(value['BSM定价:'])
         BOND_Price.append(value['债券价值:'])
+        
         #更新每日delta值
         delta = ComputeDelta(LSM_Price,BSM_Price,Time,Price)
         Delta.append(delta)
         
-    
+        #更新每日隐含波动率sigma
+        CBond_Price = everyday_data['CBOND_PRICE'][d]
+        sigma = ComputeSigma(LSM_Price,BSM_Price,Price,Time,CBond_Price,value['债券价值:'])
+        Sigma.append(sigma)
+        
+        
     LSM_Price = np.array(LSM_Price)
     BSM_Price = np.array(BSM_Price)
     BOND_Price = np.array(BOND_Price)
     Delta = np.array(Delta)
+    Sigma = np.array(Sigma)
     
-    MODEL = pd.DataFrame(data=LSM_Price,index=everyday_data.columns[-2:])
+    MODEL = pd.DataFrame(data=LSM_Price,index=everyday_data.index[-2:])
     MODEL.columns = ['LSM_PRICE']
     MODEL['BSM_PRICE'] = BSM_Price
     MODEL['BOND_PRICE'] = BOND_Price
     MODEL['DELTA'] = Delta
+    MODEL['SIGMA']  = Sigma
     
     return MODEL
 
+
+
 ##################################################################################
-
-
 
 
 
@@ -197,10 +235,10 @@ def ExecuteModel(Time,Price,Bond_Coupon,Number,everyday_data):
 Bond_Coupon,Time,Price,Number = BasicParameters(cbond_parameter)
 
 #全局动态变量(因变量)
-everyday_data = MarkToMarket(nationbond,stock_price,Time)#转债上市以来的正股价格和波动率
+everyday_data = MarkToMarket(nationbond,stock_price,cbond_price,Time)#转债上市以来的正股价格和波动率
 everyday_data = everyday_data.set_index('TRADE_DT')
-everyday_data = everyday_data.T
 
+everyday_data
 
 model = ExecuteModel(Time,Price,Bond_Coupon,Number,everyday_data)
 
